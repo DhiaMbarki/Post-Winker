a6267f0e257c95eb3cabc98e8d85d5b8ebaae42e
DhiaMbarki <tahridhia22@gmail.com>
2021-01-20 17:10:36 +0100

    create components and add files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
-
-import PostWinker from './images/PostWinker.jpg';
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import PostWinker from './images/PostWinker.png';
 const App = () => {
     return (
         <Container maxidth="lg">
@@ -9,6 +10,18 @@ const App = () => {
                 <Typography variant="h2" align = "center">PostWinker</Typography>
                 <img src = {PostWinker} alt = "PostWinker" height = "60" />
             </AppBar>
+            <Grow in>
+                <container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={4}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form/>
+                        </Grid>
+                    </Grid>
+                </container>
+            </Grow>
         </Container>
     )
 
