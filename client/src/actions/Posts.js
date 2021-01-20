9c2c257c5ffe280df60db4a36c7fdb09263a4072
DhiaMbarki <tahridhia22@gmail.com>
2021-01-20 23:30:57 +0100

    refactor

--- client/src/actions/Posts.js
+++ client/src/actions/Posts.js
@@ -2,3 +2,13 @@ import * as api from '../api' // '*' aloud us to import everything from the acti
 
 
 //Action Creators
+export const getPosts = () => async (dispatch) => {
+    try{
+        const {data} = await api.fetchPosts();
+        
+        dispatch({ type: 'FETCH_ALL'? payload: data });
+
+    }catch (error) {
+        console.log(error,message);
+    }
+}
