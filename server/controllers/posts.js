6b3dd9e0f612cc108ceece9008731684e06ac314
DhiaMbarki <tahridhia22@gmail.com>
2021-01-20 14:21:53 +0100

    create get posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,6 +1,20 @@
 // to keep our work more clean and easy we past all the routes in the post of controllers
+import PostMessage from '../models/postMessage.js'
 
-export const getPosts = (req, res) => {
-    res.send('Dude this work!')
 
+export const getPosts = async (req, res) => { //return empty array from the sckema
+    try {
+        const postMessages = await PostMessage.find();
+
+        res.status(200).json(postMessages);
+    } catch (error) {
+
+        res.status(404).json({ message: error.message});
+
+    }
+
+}
+
+export const createPost = (req, res) => {
+    res.send('Post Creation')
 }
