d0cee2b42f230dc938d8b988d9b0fd143728bc7c
DhiaMbarki <tahridhia22@gmail.com>
2021-01-21 18:03:44 +0100

    form complete

--- server/index.js
+++ server/index.js
@@ -6,13 +6,13 @@ import postRoutes from './routes/posts.js';
 
 const app = express()
 
-app.use('/posts', postRoutes);
+
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 
- 
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://javascriptmastery:javascriptmastery123@crud.c1rxu.mongodb.net/<dbname>?retryWrites=true&w=majority'
 const PORT = process.env.Port || 5000;
