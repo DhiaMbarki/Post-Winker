b5361727e1ce76dc91ac726311452581524abc01
DhiaMbarki <tahridhia22@gmail.com>
2021-01-21 15:18:36 +0100

    fixing form

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Post from './Post/Post'
-const classes = useStyles();
+import useStyles from './styles'
 
+import {useSelector} from 'react-redux';
+import useStyles from './styles'
 
 const Posts = () => {
     const classes = useStyles();
+    const posts = useSelector((state) => state.posts);
 
+    console.log(posts)
     return(
         <>
         <h1>Posts</h1>
