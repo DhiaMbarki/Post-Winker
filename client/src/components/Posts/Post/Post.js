05913ba38004b46aa6dbf33b38b5c0943c3ba4d7
DhiaMbarki <tahridhia22@gmail.com>
2021-01-22 18:43:24 +0100

    Delete button working

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -43,7 +43,7 @@ const Post = ({ post, setCurrentId }) => {
             Like
             {post.likeCount}
              </Button>
-             <Button size="small" color="primary" onClick={() => dispatch(post._id)}>
+             <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
             <DeleteIcon fontSize="small"/>
             Delete
              </Button>
